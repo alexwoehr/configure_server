21236fe3a2ed918181c03e4e45b051fc601c2d66
Alex Woehr <awwoehr@gmail.com>
2014-08-22 00:29:22 -0400

    install.sh: option to move directory and symlink original location; ensure that git directory is preserved

--- install.sh
+++ install.sh
@@ -8,17 +8,28 @@
 source ./setup_vars.sh \
 || (echo "Cannot find setup_vars.sh. Exiting..." && exit 3)
 
-if [[ -n "$1" ]]; then
+if [[ -n $1 ]]; then
   SETUP_DIR="$1"
 else
   SETUP_DIR=`pwd`
+fi
+
+if [[ -n $2 ]]; then
+  REPLACE=1
+else
+  REPLACE=""
 fi
 
 # Setup LIB_DIR if necessary
 if [ "$SETUP_DIR" != "$LIB_DIR" ]; then
   echo "Moving contents to LIB_DIR ($LIB_DIR)..."
   mkdir --parents "$LIB_DIR"
-  cp -rf "$SETUP_DIR"/* "$LIB_DIR"
+  if [[ -n $REPLACE ]]; then
+    mv "$SETUP_DIR" "$LIB_DIR"
+    ln -s "$LIB_DIR" "$SETUP_DIR"
+  else
+    cp -rf "$SETUP_DIR"/{.[a-zA-Z]*,*} "$LIB_DIR"
+  fi
 fi
 
 # Setup TMP_DIR
