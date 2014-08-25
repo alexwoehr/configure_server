6e29099e0f81154151bebbf2897453123707ca0c
Alex Woehr <awwoehr@gmail.com>
2014-08-25 16:04:52 -0400

    install_varnish_chroot: check in original version, ready for testing, without any prompts

---
+++ install_varnish_chroot.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# References: see this file:
+# - https://github.com/amedeos/UVarnishChroot/blob/master/varnish-chroot.sh
+
+# Load libraries
+source ./ui.inc
+source ./functions.inc
+
+# Setup variables
+source ./setup_vars.sh \
+  || (echo "Cannot find setup_vars.sh. Exiting..." && exit 3)
+
+readonly CHROOT_NAME="varnish"
+readonly CHROOT_JAIL_DIR=/chroot/"$CHROOT_NAME"
+readonly CHROOT_LOOP_FILE=/chroot/Loops/"$CHROOT_NAME".loop
+
+readonly USER=varnish
+readonly CHROOT_USER=chroot_varnish
+
+# Build default chroot jail
+yes | build_chroot_jail.sh "$CHROOT_NAME"
+
+# install packages for varnish
+rpm --nosignature -i https://repo.varnish-cache.org/redhat/varnish-4.0.el6.rpm
+
+yum --assumeyes install varnish \
+| ui_escape_output "yum"
+
+# Copy over sample conf files
+if [[ -e "$LIB_DIR"/samples/"$CHROOT_NAME".defaults ]]; then
+  cp -v "$LIB_DIR"/samples/"$CHROOT_NAME".defaults "$CHROOT_JAIL_DIR"/etc/sysconfig/varnish
+fi
+
+if [[ -e "$LIB_DIR"/samples/"$CHROOT_NAME".default.vcl ]]; then
+  cp -v "$LIB_DIR"/samples/"$CHROOT_NAME".default.vcl "$CHROOT_JAIL_DIR"/etc/varnish/default.vcl
+fi
+
+# Step down privileges and initiate the chrooted daemon
+cd $CHROOT_JAIL_DIR \
+&& chroot "${CHROOT_JAIL_DIR}" su "$CHROOT_USER" /sbin/service varnish start
+
+# TODO: Create options for--
+# - do not create dev (? does varnish need dev and proc?)
+# - do not copy executables
+# - setup daemon things automatically
+
