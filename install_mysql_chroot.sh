9a5b4848fbef09b47dd08daf757c9ffe30ba2156
Alex Woehr <awwoehr@gmail.com>
2014-08-27 11:19:49 -0400

    install_mysql_chroot: fixed annoying end of line / heredoc bug

--- install_mysql_chroot.sh
+++ install_mysql_chroot.sh
@@ -23,9 +23,9 @@ popd
 # mysqld expects to find this
 cp {,/chroot/mysql}/etc/sysconfig/network
 
-# install packages for varnish
-chroot "$CHROOT_JAIL_DIR" <<END_COMMANDS | ui_escape_output "yum"
-  yum --assumeyes install mysql55{,-{devel,libs,server}} \
+# install packages for mysql
+chroot "$CHROOT_JAIL_DIR" bash --login <<END_COMMANDS | ui_escape_output "yum"
+  yum --assumeyes install mysql55{,-{devel,libs,server}}
 END_COMMANDS
 
 # Copy over sample conf files
@@ -56,16 +56,17 @@ chroot . <<"END_CMDS"
 END_CMDS
 
 # Change lock file in init script to use different file
-change_line=$(grep --line-number --fixed-strings --line-regexp -e 'lockfile=/var/lock/subsys/$prog' /etc/init.d/mysqld | cut -d: -f1 )
-readonly new_value='lockfile=/var/lock/subsys/mysql-container/mysqld'
+readonly search_value='lockfile=/var/lock/subsys/$prog'
+readonly new_value='lockfile=/var/lock/subsys/mysql-container/mysqld # line changed by install_mysql_chroot.sh'
+change_line=$(grep --line-number --fixed-strings --line-regexp -e "$search_value" etc/init.d/mysqld | cut -d: -f1 | head -1 )
 sed --in-place "${change_line}c\\\n$new_value" etc/init.d/mysqld
 
 # Step down privileges and initiate mysql daemon
 cd "$CHROOT_JAIL_DIR" \
-&& chroot --userspec="$USER" "$CHROOT_JAIL_DIR" /sbin/service mysqld start
+&& chroot --userspec="$USER" "$CHROOT_JAIL_DIR" service mysqld start
 
 # TODO: Create options for--
-# - do not create dev (mysql does not need dev and proc)
+# - do not create dev (? does mysql need dev and proc?)
 # - do not copy executables
 # - setup daemon things automatically
 
