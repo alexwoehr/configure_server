19d1f367aba3f47e3c012325159f1c90646ae899
Alex Woehr <awwoehr@gmail.com>
2014-08-29 02:04:40 -0400

    archive_an_account: handling of mysql

--- archive_an_account.sh
+++ archive_an_account.sh
@@ -36,16 +36,6 @@ eval "cp /etc/httpd/sites/*$ACCOUNT* $ACCOUNT-account/httpd/sites/$ACCOUNT"
 eval "cp /etc/pki/tls/certs/*$ACCOUNT* $ACCOUNT-account/tls/certs/"
 eval "cp /etc/pki/tls/private/*$ACCOUNT* $ACCOUNT-account/tls/private/"
 
-# TODO: dump mysql tables, copy over varnish configuration
-#### Dump and save mysql tables
-# Use chroot if available
-#### if [[ -e /chroot/mysql ]]; then
-####   pushd /chroot/mysql
-#### else
-####   pushd /
-#### fi
-#### chroot . mysqldump -u root -p --skip-lock-tables smf > ~-/$ACCOUNT-account/mysql/smf.sql
-
 # Copy varnish configuration over
 # Use chroot if available
 if [[ -e /chroot/varnish ]]; then
@@ -58,6 +48,20 @@ cp -r etc/varnish/$ACCOUNT ~-/$ACCOUNT-account/varnish/
 
 popd
 
+# TODO: dump mysql tables, copy over varnish configuration
+#### Dump and save mysql tables
+# Use chroot if available
+#### if [[ -e /chroot/mysql ]]; then
+####   pushd /chroot/mysql
+#### else
+####   pushd /
+#### fi
+#### chroot . mysqldump -u root -p --skip-lock-tables smf > ~-/$ACCOUNT-account/mysql/smf.sql
+#### popd
+echo "Please dump mysql and hit enter when you return"
+echo "Something like: chroot . mysqldump -u root -p --skip-lock-tables smf > ~-/$ACCOUNT-account/mysql/smf.sql"
+read proceed
+
 # Compress and encrypt the directory
 tar cf "$ACCOUNT"-account{.tar,}
 xz "$ACCOUNT"-account.tar
@@ -67,13 +71,7 @@ cat "$ACCOUNT"-account.tar.xz | gpg --symmetric --batch --passphrase="$ENCRYPTIO
 # 
 #rm -rf "$ACCOUNT"-account{.tar{.xz,},/}
 
-pushd /chroot/mysql/root
-unxz 3foldx.sql.gpg.xz
-gpg --decrypt --batch --passphrase="$ENCRYPTION_KEY" 3foldx.sql.gpg
-popd
-
 echo "Account tarball generated. Saved to $ACCOUNT-account.tar.xz.gpg"
 
 # Standard directories in an account tarball
 # mkdir --parents "$ACCOUNT"-account/{httpd/sites/"$ACCOUNT",tls/{certs,private},srv/$ACCOUNT/{logs,tmp,www.$ACCOUNT.com,testing.$ACCOUNT.com,htpasswds,notes,ftp,archives}}
-
